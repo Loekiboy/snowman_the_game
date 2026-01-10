4ee47af5d284611e48702d94dc0561b3420a6b91
Loekiboy <loekoerlemans@gmail.com>
2026-01-10 17:59:05 +0100

    improved the animations

--- platformer-hackclub/character_body_2d.gd
+++ platformer-hackclub/character_body_2d.gd
@@ -4,10 +4,9 @@ extends CharacterBody2D
 const SPEED = 70.0
 const JUMP_VELOCITY = -230.0
 
-# --- VARIABELEN VOOR DE TELLER ---
+# --- VARIABELEN ---
 var aangeraakte_tiles = []
 var count = 0
-
 
 # onready vars
 @onready var label = get_node("../UI/TileCounterLabel")
@@ -16,19 +15,17 @@ var count = 0
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var idle_timer = $IdleTimer
 
-var idle_stage = 0
-
 func _ready():
-	# Dit wordt één keer uitgevoerd als het spel start
-	if popup:
-		popup.visible = false
+	if popup: popup.visible = false
+	if teller_label: teller_label.visible = true
 	
-	# Zorg dat je teller aan het begin wel zichtbaar is
-	if teller_label:
-		teller_label.visible = true
+	# Start direct met wachten als het spel begint
+	_animated_sprite.play("idle")
+	_animated_sprite.frame = 0
+	idle_timer.start(3.0)
 
 func _physics_process(delta: float) -> void:
-	# 1. Zwaartekracht toepassen
+	# 1. Zwaartekracht
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -36,144 +33,91 @@ func _physics_process(delta: float) -> void:
 	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# 3. Beweging berekenen
+	# 3. Beweging links/rechts
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	# 4. Animaties en Idle logica
-	if velocity.x != 0:
-		_animated_sprite.play("walk")
-		idle_stage = 0
-		idle_timer.stop()
+	
+	# 4. ANIMATIE LOGICA
+	if not is_on_floor():
+		idle_timer.stop() # Geen idle timer tijdens springen/vallen
 		
-		if velocity.x < 0:
-			_animated_sprite.flip_h = true
+		if velocity.y < 0:
+			_animated_sprite.play("jump")
 		else:
-			_animated_sprite.flip_h = false
+			_animated_sprite.play("fall")
 	else:
-		if idle_timer.is_stopped():
-			_run_idle_logic()
+		if velocity.x != 0:
+			_animated_sprite.play("walk")
+			idle_timer.stop()
+			if velocity.x < 0:
+				_animated_sprite.flip_h = false
+			else:
+				_animated_sprite.flip_h = true
+		else:
+	# Alleen als we NET gestopt zijn met lopen
+			if _animated_sprite.animation == "walk":
+				_animated_sprite.play("idle")
+				_animated_sprite. frame = 0
+				idle_timer.start(3.0)
+			if _animated_sprite.animation == "fall":
+				_animated_sprite.play("idle")
+				_animated_sprite. frame = 0
+				idle_timer.start(3.0)
+	# Als we al in idle zijn, doe NIETS - laat de timer zijn werk doen
 
-	# 5. DAADWERKELIJK BEWEGEN (Moet altijd in physics_process staan!)
+	# 5. Bewegen
 	move_and_slide()
 
-	# 6. TILE DETECTIE (Na het bewegen)
+	# 6. Tile detectie
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
-		
 		if collider is TileMapLayer:
 			var botspunt = collider.to_local(collision.get_position() - collision.get_normal() * 1)
 			var tile_coord = collider.local_to_map(botspunt)
-			
 			if collider.get_cell_source_id(tile_coord) != -1:
 				if not tile_coord in aangeraakte_tiles:
 					aangeraakte_tiles.append(tile_coord)
 					count += 1
 					update_ui()
-					
 					var snow_layer = get_node("../SnowLayer")
 					if snow_layer:
-						var snow_atlas_pos = Vector2i(16, 7)
-						snow_layer.set_cell(tile_coord, 2, snow_atlas_pos)
-	
-	# Check of de speler beweegt
+						snow_layer.set_cell(tile_coord, 2, Vector2i(16, 7))
+
+# --- DE SIMPELE IDLE LOGICA ---
+# Deze functie wordt ALLEEN uitgevoerd als de Timer afloopt (timeout)
+func _on_idle_timer_timeout() -> void:
+	# Als we bewegen, doen we niks
 	if velocity.x != 0:
-		# Als we lopen, resetten we alles
-		_animated_sprite.play("walk")
-		idle_stage = 0
-		idle_timer.stop()
-		
-		if velocity.x < 0:
-			_animated_sprite.flip_h = false
-		else:
-			_animated_sprite.flip_h = true
-	else:
-		# Als we stilstaan en de timer loopt nog niet, start de wacht-cyclus
-		if idle_timer.is_stopped():
-			_run_idle_logic()
+		return
 
-func _run_idle_logic():
-	if idle_stage == 0:
-		# Toon alleen het eerste frame (index 0)
-		_animated_sprite.play("idle")
-		_animated_sprite.stop() # Stop direct op frame 0
-		_animated_sprite.frame = 0
-		idle_stage = 1
-		idle_timer.start(3.0) # Wacht precies 3 seconden
+	# We kijken welk frame er NU aanstaat
+	var frame_nu = _animated_sprite. frame
 	
-	elif idle_stage == 1:
-		# Speel de volgende 2 frames
-		_animated_sprite.play("idle")
-		# We wachten tot hij bij frame 2 is en stoppen hem dan
-		# Dit doen we via een signaal of simpelweg na een korte tijd
-		idle_stage = 2
-		idle_timer.start(randf_range(1.0, 3.0)) # Wacht tussen 1 en 3 sec
-		
-	elif idle_stage == 2:
-		# Volgende frames...
-		# Je kunt hier herhalen wat je wilt
-		idle_stage = 0 # Ga terug naar begin
+	if frame_nu == 0:
+		# We kijken naar voren (0), kies nu een zijkant
+		var keuzes = [1, 2]
+		_animated_sprite.frame = keuzes.pick_random()
+		# Wacht 1 tot 3 seconden op de zijkant
 		idle_timer.start(randf_range(1.0, 3.0))
+	else:
+		# We keken opzij (1 of 2), ga nu terug naar het midden
+		_animated_sprite.frame = 0
+		# Wacht verplicht 3 seconden in het midden voor rust
+		idle_timer.start(3.0)
 
-	# 4. Bewegen
-	move_and_slide()
-
-	# 5. TILE DETECTIE
-	# We checken alle botsingen die move_and_slide zojuist heeft gevonden
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
-		
-		if collider is TileMapLayer:
-			# We kijken 4 pixels "in" de tegel om zeker te weten dat we hem raken
-			var botspunt = collider.to_local(collision.get_position() - collision.get_normal() * 1)
-			var tile_coord = collider.local_to_map(botspunt)
-			
-			# Check of de tegel niet leeg is
-			if collider.get_cell_source_id(tile_coord) != -1:
-				# Alleen tellen als we dit specifieke grid-vakje nog niet hebben gehad
-				if not tile_coord in aangeraakte_tiles:
-					aangeraakte_tiles.append(tile_coord)
-					count += 1
-					update_ui()
-					
-					var layer = get_node("../TileMapLayer")
-					if layer:
-						
-						var snow_layer = get_node("../SnowLayer")
-						if snow_layer:
-							# We gebruiken tile_coord in plaats van (16, 18)
-							# Zo komt de sneeuw onder je voeten terecht!
-							var snow_atlas_pos = Vector2i(16, 7)
-							snow_layer.set_cell(tile_coord, 2, snow_atlas_pos)
 # Functie om de tekst op je scherm aan te passen
 func update_ui():
 	if label:
 		label.text = "Tiles touched: " + str(count)
-	else:
-		# Dit verschijnt onderin in je 'Output' venster als het label niet gevonden wordt
-		print("Fout: Ik kan de Label node niet vinden!")
 
 func _on_finish_flag_body_entered(body: Node2D) -> void:
-	# Check of het echt de speler is (CharacterBody2D) die de vlag raakt
 	if body.name == "CharacterBody2D":
-
-		if teller_label:
-			teller_label.visible = false
-			
+		if teller_label: teller_label.visible = false
 		if popup:
-			var win_label = popup.get_node("TextEdit") 
-			if win_label:
-				win_label.text = "Tiles touched: " + str(count)
-			
+			var win_label = popup.get_node("TextEdit")
+			if win_label: win_label.text = "Tiles touched: " + str(count)
 			popup.visible = true
-			
-
-
-func _on_idle_timer_timeout() -> void:
-	if velocity.x == 0:
-		_run_idle_logic()
