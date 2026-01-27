ac657ff3d6cc62cd1a6b390ace3a1707f74109aa
Loekiboy <loekoerlemans@gmail.com>
2026-01-27 18:06:55 +0100

    level 2 + better snow placement(not finsihed)

--- platformer-hackclub/character_body_2d.gd
+++ platformer-hackclub/character_body_2d.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
-# max tiles level 1: 605
+
 # --- JOUW INSTELLINGEN ---
 const SPEED = 70.0
 const JUMP_VELOCITY = -230.0
 const COYOTE_TIME = 0.16
 const CLIMB_SPEED = -70
+const FOOTPRINT_DISTANCE = 8.0 # Hoeveel pixels lopen voor nieuwe sneeuw
 
 # --- VARIABELEN ---
 var aangeraakte_tiles = []
@@ -14,20 +15,20 @@ var is_on_ladder = false
 var just_jumped = false 
 var time_elapsed = 0.0
 var level_active = true
+var last_snow_pos = Vector2.ZERO
 
 # Idle animatie variabelen
 var idle_target_frame = 0
 var idle_in_transition = false
-var first_idle = true  # Track of dit de eerste idle is
+var first_idle = true 
 
 # onready vars
 @onready var label = get_node("../UI/TileCounterLabel")
 @onready var stopwatch_label = get_node("/root/Main/UI/StopwatchLabel")
 @onready var popup = get_node("/root/Main/UI/WinPopup")
 @onready var teller_label = get_node("/root/Main/UI/TileCounterLabel")
-@onready var restart_button = get_node("/root/Main/UI/WinPopup/Button")
-@onready var next_level_button = get_node("/root/Main/UI/WinPopup/NextLevelButton")
 @onready var tilemap_layer = get_node("../TileMapLayer2")
+@onready var snow_layer = get_node("../SnowLayer") # Je TileMapLayer voor de textures
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var idle_timer = $IdleTimer
 @onready var snow_step_player = $SnowStepPlayer
@@ -43,53 +44,21 @@ var snow_step_sfx := [
 ]
 
 func _ready():
-	if popup:  popup.visible = false
+	if popup: popup.visible = false
 	if teller_label: teller_label.visible = true
-	
-	if restart_button:
-		restart_button.pressed.connect(_on_restart_button_pressed)
-		
-	if next_level_button:
-		next_level_button.pressed.connect(_on_next_level_button_pressed)
-	
 	_animated_sprite.play("idle")
-	_animated_sprite.frame = 0
-	# NIET hier starten, wachten tot speler stilstaat
-	
 	RenderingServer.set_default_clear_color(Color("#c2e3e8"))
 	randomize()
 
 func _physics_process(delta: float) -> void:
-	
 	if level_active:
 		time_elapsed += delta
 		_update_stopwatch_ui()
-	# 1. Ladder check
-	is_on_ladder = false
-	var parent = get_parent()
-	if parent: 
-		# Pas deze aan als je tilesize anders is dan 16x16
-		var tile_size = 18
-		# De offset die je noemde: 19 tiles naar rechts (+), 13 naar beneden (+)
-		var ladder_offset = Vector2(14.5 * tile_size, 17.4 * tile_size)
 
-		for child in parent.get_children():
-			if child is TileMapLayer: 
-				var check_positions = [
-					global_position + ladder_offset,
-					global_position + ladder_offset + Vector2(0, 8),
-					global_position + ladder_offset + Vector2(0, -8),
-				]
-				for check_pos in check_positions:
-					var tile_pos = child.local_to_map(check_pos)
-					var tile_data = child.get_cell_tile_data(tile_pos)
-					if tile_data and tile_data.get_custom_data("is_climbable"):
-						is_on_ladder = true
-						break
-				if is_on_ladder:   
-					break
+	# 1. Ladder check (vereenvoudigd voor leesbaarheid)
+	_check_ladder()
 
-	# 2. Zwaartekracht & Coyote Reset
+	# 2. Zwaartekracht
 	if is_on_floor():
 		coyote_timer = COYOTE_TIME
 		just_jumped = false
@@ -101,205 +70,129 @@ func _physics_process(delta: float) -> void:
 			velocity += get_gravity() * delta
 			coyote_timer -= delta
 
-	# 3. SPRINGEN
-	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")) \
-		and coyote_timer > 0 \
-		and not is_on_ladder:
-		velocity.y = JUMP_VELOCITY
-		coyote_timer = 0
-		just_jumped = true
+	# 3. Springen & 4. Klimmen & 5. Beweging
+	_handle_input()
 
-	# 4. KLIMMEN
-	if is_on_ladder and not just_jumped:
-		if Input.is_action_pressed("ui_up"):
-			velocity.y = CLIMB_SPEED
-		elif Input.is_action_pressed("ui_down"):
-			velocity.y = -CLIMB_SPEED
-		else:
-			velocity.y = 0
-
-	if just_jumped and velocity.y > 0:
-		just_jumped = false
-
-	# 5. Beweging links/rechts
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-# 6. ANIMATIE LOGICA
-	if is_on_ladder and not is_on_floor():
-		_animated_sprite.play("climb")
-		# Pauzeer de animatie als de speler niet omhoog of omlaag beweegt
-		if velocity.y == 0:
-			_animated_sprite.stop() 
-		else:
-			_animated_sprite.play()
-			
-		idle_timer.stop()
-		idle_in_transition = false
-		first_idle = true
-
-	elif not is_on_floor():
-		idle_timer.stop()
-		idle_in_transition = false
-		first_idle = true
-		if velocity.y < 0:
-			_animated_sprite.play("jump")
-		else:
-			_animated_sprite.play("fall")
-	else:
-		
-		if velocity.x != 0:
-			_animated_sprite.play("walk")
-			idle_timer.stop()
-			idle_in_transition = false
-			first_idle = true
-			if velocity.x < 0:
-				_animated_sprite.flip_h = false
-			else:  
-				_animated_sprite.flip_h = true
-		
-		elif Input.is_action_pressed("ui_down"):
-			_animated_sprite.play("crouch")
-			idle_timer.stop()
-			idle_in_transition = false
-			first_idle = true
-		
-		else:
-			# Speler staat stil -> start idle animatie
-			if _animated_sprite.animation != "idle":
-				_animated_sprite.play("idle")
-				_animated_sprite.frame = 0
-				idle_in_transition = false
-				first_idle = true
-				idle_timer.start(3.0)  # EERSTE keer 3 seconden
-			elif first_idle and not idle_timer.is_stopped():
-				# Idle is al aan het lopen, doe niets
-				pass
+	# 6. Animatie
+	_handle_animations()
 
 	# 7. Bewegen
 	move_and_slide()
 
-	# 8. Tile detectie
+	# 8. DE PIXEL-PRECIES SNEEUW LOGICA
+	_handle_snow_generation()
+
+func _handle_snow_generation():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
-	
+		
 		if collider is TileMapLayer:
 			var normal = collision.get_normal()
-			
+			# Alleen als we op de grond staan
 			if normal.y < -0.5:
-				var botspunt = collider.to_local(collision.get_position() - normal * 1)
-				var tile_coord = collider.local_to_map(botspunt)
+				var collision_pos = collision.get_position()
 				
-				if collider.get_cell_source_id(tile_coord) != -1:
-					if not tile_coord in aangeraakte_tiles: 
-						aangeraakte_tiles.append(tile_coord)
-						count += 1
-						update_ui()
-						var snow_layer = get_node("../SnowLayer")
-						if snow_layer: 
-							snow_layer.set_cell(tile_coord, 2, Vector2i(16, 7))
+				# Check of we ver genoeg bewogen hebben
+				if collision_pos.distance_to(last_snow_pos) > FOOTPRINT_DISTANCE:
+					_spawn_hybrid_snow(collision_pos, collider)
+					last_snow_pos = collision_pos
 
-							# plays rondom snow effect
-							if snow_step_player and snow_step_sfx.size() > 0:
-								snow_step_player.stream = snow_step_sfx[randi_range(0, snow_step_sfx.size() - 1)]
-								snow_step_player.play()
+func _spawn_hybrid_snow(pos: Vector2, original_tilemap: TileMapLayer):
+	# 1. BEREKEN DE X OP HET GRID
+	# We pakken de X van de tilemap en snappen de wereld-X daarop
+	var local_pos = original_tilemap.to_local(pos)
+	var map_coord = original_tilemap.local_to_map(local_pos)
+	var snapped_x = original_tilemap.to_global(original_tilemap.map_to_local(map_coord)).x
+	
+	# 2. MAAK EEN SPRITE DIE LIJKT OP DE TILE
+	var snow_sprite = Sprite2D.new()
+	
+	# We halen de texture uit je bestaande SnowLayer TileSet
+	var tileset = snow_layer.tile_set
+	var source = tileset.get_source(2) as TileSetAtlasSource # Jouw source ID 2
+	
+	snow_sprite.texture = source.texture
+	# We pakken precies het vierkantje van de sneeuw-tile (Vector2i(16, 7))
+	var region_size = tileset.tile_size
+	snow_sprite.region_enabled = true
+	snow_sprite.region_rect = Rect2(Vector2(16 * region_size.x, 7 * region_size.y), region_size)
+	
+	# 3. POSITIONERING
+	# X is snapped naar het grid, Y is precies waar de collision was
+	snow_sprite.global_position = Vector2(snapped_x, pos.y)
+	snow_sprite.z_index = -1 # Achter de speler
+	
+	get_parent().add_child(snow_sprite)
+	
+	# 4. SCORE EN GELUID
+	_update_score(map_coord)
+	
+	if snow_step_player and snow_step_sfx.size() > 0:
+		snow_step_player.stream = snow_step_sfx.pick_random()
+		snow_step_player.play()
 
-func _on_idle_timer_timeout() -> void:
-	# Stop als speler beweegt
-	if velocity.x != 0:
-		idle_in_transition = false
-		first_idle = true
-		return
+func _update_score(tile_coord: Vector2i):
+	if not tile_coord in aangeraakte_tiles:
+		aangeraakte_tiles.append(tile_coord)
+		count += 1
+		update_ui()
 
-	var frame_nu = _animated_sprite.frame
-	
-	# Als we in een transitie zitten, ga naar het doel frame
-	if idle_in_transition:
-		_animated_sprite.frame = idle_target_frame
-		idle_in_transition = false
-		idle_timer.start(randf_range(1.0, 3.0))
-		return
-	
-	# EERSTE keer (3 sec omhoog gekeken) -> kies nu links of rechts
-	if first_idle: 
-		first_idle = false
-		var keuzes = [1, 2]  # Links of rechts
-		_animated_sprite.frame = keuzes.pick_random()
-		idle_timer.start(randf_range(1.0, 3.0))
-		return
-	
-	# We kijken naar links (1) of rechts (2)
-	if frame_nu == 1 or frame_nu == 2:
-		var keuzes = [0, 1, 2]
-		keuzes.erase(frame_nu)  # Verwijder huidige frame uit keuzes
-		var nieuw_frame = keuzes.pick_random()
-		
-		# Als we van links naar rechts gaan (of andersom), eerst via midden
-		if (frame_nu == 1 and nieuw_frame == 2) or (frame_nu == 2 and nieuw_frame == 1):
-			_animated_sprite.frame = 0
-			idle_target_frame = nieuw_frame
-			idle_in_transition = true
-			idle_timer.start(0.2)  # Korte pauze in het midden
-		else:
-			_animated_sprite.frame = nieuw_frame
-			idle_timer.start(randf_range(1.0, 3.0))
-	
-	# We kijken naar voren (frame 0) -> wacht nog niet, kies volgende
+# --- HULPFUNCTIES (De rest van je code) ---
+
+func _check_ladder():
+	is_on_ladder = false
+	var parent = get_parent()
+	if parent: 
+		var tile_size = 18
+		var ladder_offset = Vector2(14.5 * tile_size, 17.4 * tile_size)
+		for child in parent.get_children():
+			if child is TileMapLayer: 
+				var check_pos = global_position + ladder_offset
+				var tile_pos = child.local_to_map(check_pos)
+				var tile_data = child.get_cell_tile_data(tile_pos)
+				if tile_data and tile_data.get_custom_data("is_climbable"):
+					is_on_ladder = true
+					break
+
+func _handle_input():
+	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")) \
+		and coyote_timer > 0 and not is_on_ladder:
+		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0
+		just_jumped = true
+
+	if is_on_ladder and not just_jumped:
+		if Input.is_action_pressed("ui_up"): velocity.y = CLIMB_SPEED
+		elif Input.is_action_pressed("ui_down"): velocity.y = -CLIMB_SPEED
+		else: velocity.y = 0
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction: velocity.x = direction * SPEED
+	else: velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func _handle_animations():
+	if is_on_ladder and not is_on_floor():
+		_animated_sprite.play("climb")
+		if velocity.y == 0: _animated_sprite.stop()
+		else: _animated_sprite.play()
+	elif not is_on_floor():
+		if velocity.y < 0: _animated_sprite.play("jump")
+		else: _animated_sprite.play("fall")
 	else:
-		var keuzes = [1, 2]
-		_animated_sprite.frame = keuzes.pick_random()
-		idle_timer.start(randf_range(1.0, 3.0))
+		if velocity.x != 0:
+			_animated_sprite.play("walk")
+			_animated_sprite.flip_h = velocity.x > 0
+		elif Input.is_action_pressed("ui_down"):
+			_animated_sprite.play("crouch")
+		else:
+			if _animated_sprite.animation != "idle":
+				_animated_sprite.play("idle")
 
 func update_ui():
-	if label:
-		label.text = "Tiles touched: " + str(count)
+	if label: label.text = "Tiles touched: " + str(count)
 
-func _on_finish_flag_body_entered(body: Node2D) -> void:
-	if body.name == "CharacterBody2D":
-		level_active = false # Stop de timer
-		
-		if teller_label: teller_label.visible = false
-		if stopwatch_label: stopwatch_label.visible = false
-		
-		if popup: 
-			var win_label = popup.get_node("TextEdit")
-			if win_label:
-				# 1. Bereken de multiplier en de eindscore als getallen
-				var multiplier = time_elapsed / 10.0
-				var totaal_score = count * multiplier
-				
-				# 2. Rond de getallen af voor een mooie weergave
-				var afgeronde_mult = snapped(multiplier, 0.1)
-				var afgeronde_score = snapped(totaal_score, 1) # Score afronden op heel getal
-				
-				# 3. Zet alles pas in de tekst (we gebruiken str() alleen om het te laten zien)
-				win_label.text = "Tiles: " + str(count) + " x " + str(afgeronde_mult) + \
-								 "\nScore: " + str(afgeronde_score)
-				
-				
-			popup.visible = true
-			
-func _on_restart_button_pressed():
-	get_tree().reload_current_scene()
-	
-func _on_next_level_button_pressed():
-	var next_level = "res://main2.tscn"
-	
-	if FileAccess.file_exists(next_level):
-		get_tree().change_scene_to_file(next_level)
-	else:
-		print("Fout: Level 2 niet gevonden!")
-	
 func _update_stopwatch_ui():
 	if stopwatch_label:
 		var multiplier = time_elapsed / 10.0
-		
-		# We maken een mooie tekstregel: "TIJD | MULTIPLIER"
-		# De snapped(multiplier, 0.1) zorgt voor 1 cijfer achter de komma
-		var multiplier_tekst = "x" + str(snapped(multiplier, 0.1))
-		
-		stopwatch_label.text = multiplier_tekst
+		stopwatch_label.text = "x" + str(snapped(multiplier, 0.1))
